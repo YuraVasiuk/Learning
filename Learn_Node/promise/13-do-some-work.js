d1484347d65cf5eb3e121619b2ee99bd1413e965
YuriiVasiuk <yuravasiuk@gmail.com>
2018-01-11 17:47:33 -0700

    Learn_CSS created

--- Learn_Node/promise/13-do-some-work.js
+++ Learn_Node/promise/13-do-some-work.js
@@ -45,19 +45,3 @@ var qhttp = require('q-io/http');
     .done();
 */
 
-async function hi() {
-    try {
-        while (i < 5) {
-
-            var hi = await q_http.read(url);
-            var hi2 = await q_http.read(hi + 'asdfa');
-            i++;
-        }
-    } catch (e) {
-
-    }
-
-}
-
-hi();
-console.log('11');
--- Learn_Node/promise/5-always-async.js
+++ Learn_Node/promise/5-always-async.js
@@ -3,6 +3,10 @@
 /*eslint no-console:0*/
 'use strict';
 
+// for any promise the computer reads the file twice:
+// 1st read -- set up the pipes of the promise
+// 2nd read -- flush the activities of code through those pipes
+
 var promise = new Promise(function (fulfill, reject) {
     fulfill('PROMISE VALUE');
 });
